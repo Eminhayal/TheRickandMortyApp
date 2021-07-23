3035880c39b78eb2bebe39108aff0927f1506977
Emin Hayal <eminhayal768@gmail.com>
2021-07-23 16:57:00 +0300

    New app build

---
+++ TheRickandMortyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheRickandMortyApp
+//
+//  Created by Emin Hayal on 23.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
